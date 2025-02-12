4685da1704bf34e06dcc43a535aa6e43609d2345
Shivanshi Dwivedi <146759674+SHIVANSHIDWIVEDI@users.noreply.github.com>
2025-02-12 14:49:06 +0530

    Update Home.jsx

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -20,9 +20,9 @@ import PropTypes from "prop-types";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/woman-with-tablet.jpg";
+import image from "C:\Users\dwive\Downloads\WhatsApp Image 2025-02-12 at 14.22.36_71355d3b.jpg";
 
-const imageAltText = "Adult female in office setting leaning against a glass wall while holding a platinum Microsoft Surface Pro 7 in tablet mode preparing to write with Microsoft Surface Pen";
+const imageAltText = "A girl standing at one of her events and smiling her heart out";
 
 const Home = ({ name, title }) => {
   return (
